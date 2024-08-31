3659719ce2affe95719d0e1c141177a9beda8133
Shivambhardwaj13 <shivam.bhardwaj13@outlook.com>
2024-08-31 20:01:28 +0530

    Reverse String

---
+++ Reverse_String.java
@@ -0,0 +1,26 @@
+public class Reverse_String {
+	public static void main(String[] args) {
+
+		/*
+
+//	Using StringBuffer :
+		String s="Sam Bhardwaj";
+		StringBuilder sb = new StringBuilder(s);
+		String rev = sb.reverse().toString();
+		System.out.println(s);
+		System.out.println(rev);
+
+ */
+
+//		using Loop :
+//		String s = "Shivam Bhardwaj";
+		String s = "jawdrahB mavihS";
+
+		String rev = "";
+		for(int i = s.length()-1 ; i>=0 ; i--){
+			rev += s.charAt(i);
+		}
+		System.out.println(s);
+		System.out.println(rev);
+	}
+}
